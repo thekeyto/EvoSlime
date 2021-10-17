1a1290ac43d50432060180885a2376030403b382
LinZhaoqian <1203783169@qq.com>
2021-10-17 15:17:08 +0800

    基础关卡0.1

---
+++ Assets/Scripts/ChangeTile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class ChangeTile : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public TileBase tile;
+    public GameObject changePoint;
+    
+    public  Grid grid;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+            Vector3Int cellPos = grid.WorldToCell(changePoint .transform .position );
+            tilemap.SetTile(cellPos, tile);
+    }
+
+}
---
+++ Assets/Scripts/puzzleController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Zhuzi1;
+    public bool isSetup;
+    public GameObject lazer;
+    public GameObject checkPoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPos();
+        TargetLazer();
+    }
+    private void CheckPos()
+    {
+        if ((Zhuzi1.transform.position - checkPoint.transform.position).magnitude <0.1)
+        {
+            isSetup = true;
+        }
+        else
+        {
+            isSetup = false;
+        }
+    }
+    private void TargetLazer()
+    {
+        if (isSetup)
+        {
+            lazer.transform.localScale = new Vector3(1, 0.1f, 1);
+        }
+        else
+        {
+            lazer.transform.localScale = new Vector3(0, 0.1f, 1);
+        }
+    }
+}
